45dccab2c05d646ede6c71483247841ccf0fbda6
MarsiaRochelin <marsrochelin@pursuit.org>
2023-07-12 17:52:56 -0400

    completed code along

---
+++ server.js
@@ -0,0 +1,11 @@
+//Import the app
+//Start up and listen
+require("dotenv").config();
+
+const app = require("./app");
+
+const PORT = process.env.PORT || 9000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
